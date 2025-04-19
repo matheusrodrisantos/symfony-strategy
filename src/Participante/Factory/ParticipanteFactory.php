2960fd56734f7d7e69c3b85ec3d23aad246a504b
Matheus Rodrigues <matheus.rodrisantos@outlook.com>
2025-04-19 20:35:00 -0300

    add class factory

---
+++ src/Participante/Factory/ParticipanteFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Participante\Factory;
+
+use App\Participante\DTO\ParticipanteInputDTO;
+use App\Participante\DTO\ParticipanteOutputDTO;
+
+class ParticipanteFactory
+{
+    public function createFromDto(ParticipanteInputDTO $inputDto){
+
+    }
+
+}
